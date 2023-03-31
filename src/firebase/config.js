e73ad497055b6acabfaa17dfdc599a5d07085a99
gabyruas <gabyyy.ruas@gmail.com>
2023-03-31 15:03:03 -0300

    adicionei o botao de mais informações

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Chaves de acesso ao firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCGV-hKd1DwX0wKcmlJV65SF1V2m1Bo9eY",
-  authDomain: "bibliotech-aulas-241dd.firebaseapp.com",
-  projectId: "bibliotech-aulas-241dd",
-  storageBucket: "bibliotech-aulas-241dd.appspot.com",
-  messagingSenderId: "455282911729",
-  appId: "1:455282911729:web:37e3cca50849ce62b73c50",
+  apiKey: "AIzaSyDFeIbZWt0skyijj4D_oxalcbwL-_dUw_8",
+  authDomain: "bibliotech-gaby.firebaseapp.com",
+  projectId: "bibliotech-gaby",
+  storageBucket: "bibliotech-gaby.appspot.com",
+  messagingSenderId: "351852942822",
+  appId: "1:351852942822:web:e97b72a18c53bc63b5f227"
 };
 
 // Inicializa o app com base nas configurações acima
